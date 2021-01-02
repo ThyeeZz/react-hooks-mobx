e3cc35b90f59d5f0a4099133eafa3d19a5d54477
ThyeeZz <617619820@qq.com>
2021-01-02 23:57:18 +0800

    dev 分支 首次提交

--- src/views/Login/functions.js
+++ src/views/Login/functions.js
@@ -21,7 +21,7 @@ export const useSignup = (loginStore, props) => {
   return useCallback((user) => {
     const res = loginStore.addUser(user);
     if (res.code === 1) {
-      message.success('登录成功')
+      message.success('注册，登录成功')
       props.history.push('/index')
     } else {
       message.error(res.msg)
