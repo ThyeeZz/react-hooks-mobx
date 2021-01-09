fb1afc9653c8b31ed6d1736a871d90307aff09f5
ThyeeZz <617619820@qq.com>
2021-01-09 16:53:00 +0800

    修改 404页面逻辑

--- src/views/Error/index.jsx
+++ src/views/Error/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import logo from '../../assets/images/error.png'
 const Error = () => {
   return (
     <div style={{ width: '100%', height: '100%' }}>
-      <img src='../../assets/images/error.png' alt="" style={{ width: '100%', height: '100%', pointerEvents: 'none' }} />
+      <img src={logo} alt="" style={{ width: '100%', height: '100%', pointerEvents: 'none' }} />
     </div>
   )
 }
