1b2f1fbd9ca059a76791cbb1114d2ce08b55ad3c
ThyeeZz <617619820@qq.com>
2021-01-10 00:37:33 +0800

    fix:修复重定向之前无法匹配到正确title的问题

--- src/views/Index/useRouter.js
+++ src/views/Index/useRouter.js
@@ -2,18 +2,25 @@ import {
   useCallback,
 } from 'react';
 
-function useRouter (routes, pathname){
+function useRouter(routes, pathname) {
 
-  const getRouteNames =  useCallback((routes, pathname) => {
-    const [pathname4,pathname1, pathname2, pathname3] = pathname.split('/');
+  const getRouteNames = useCallback((routes, pathname) => {
+    const [pathname0, pathname1, pathname2, pathname3] = pathname.split('/');
+    let [levelOneLabel, levelTwoLabel] = ["", ""];
+    if (pathname2) {
+      const path1 = routes.find(() => pathname.includes(pathname2));
+      levelOneLabel = path1.label;
+      
+      if (pathname3) {
+        const path2 = path1.children.find(item => item.path.includes(pathname3));
+        levelTwoLabel = path2.label;
+      }
+    }
+    document.title = `${levelOneLabel || "React App"}-${levelTwoLabel}`
+    return [levelOneLabel, levelTwoLabel]
 
-    const path1 = routes.find(() => pathname.includes(pathname2));
-  
-    const path2 = path1.children.find(item => item.path.includes(pathname3));
 
-    document.title = `${path1.label}-${path2.label}`
-    return [path1.label, path2.label]
-  },[routes, pathname]);
+  }, [routes, pathname]);
   const routeNames = getRouteNames(routes, pathname);
 
   return routeNames
