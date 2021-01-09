ee464d2ebab50540e8e36e45443fcfc2afed099f
ThyeeZz <617619820@qq.com>
2021-01-09 16:53:45 +0800

    修改 路由配置，目前问题是几个基础Layout如何展示

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,120 @@
 import AsyncRouter from '../components/AsyncRouter';
+import {
+  AppstoreOutlined,
+  PieChartOutlined,
+  DesktopOutlined,
+  ContainerOutlined,
+  MailOutlined,
+} from '@ant-design/icons';
+import {
+  BasicLayout
+} from '../components/BasicLayout';
+import {
+  UserLayout
+} from '../components/UserLayout';
+
 const Mine = AsyncRouter(() => import('../views/system/Mine'));
 const Home = AsyncRouter(() => import('../views/system/Home'));
 const Assets = AsyncRouter(() => import('../views/system/Assets'));
 const Statistics = AsyncRouter(() => import('../views/system/Statistics'));
 
-const ROUTES = [
+
+const ROUTES = [{
+    id: '1',
+    path: "bookkeepManager",
+    label: '手账管理',
+    icon: DesktopOutlined,
+    layout: BasicLayout,
+    children: [{
+        id: '1-1',
+        path: '/bookkeepManager/mine',
+        label: '我的',
+        component: Mine
+      },
+      {
+        id: "1-2",
+        label: "明细",
+        path: '/bookkeepManager/home',
+        component: Home
+      },
+      {
+        id: '1-3',
+        path: '/bookkeepManager/assets',
+        label: "资产",
+        component: Assets
+      },
+      {
+        id: '1-4',
+        path: '/bookkeepManager/statistics',
+        label: "统计",
+        component: Statistics
+      },
+    ]
+  },
+
   {
-    id: 1,
-    path: '/mine',
-    component: Mine
+    id: '2',
+    icon: AppstoreOutlined,
+    layout: UserLayout,
+    path: "navigationTwo",
+    label: 'Navigation Two',
+    children: [{
+        id: '2-1',
+        path: '/navigationTwo/mine',
+        label: 'Option 5',
+        component: Statistics
+      },
+      {
+        id: "2-2",
+        label: "Option 6",
+        path: '/navigationTwo/home',
+        component: Statistics
+      },
+      {
+        id: '2-3',
+        path: '/navigationTwo/assets',
+        label: "Option 7",
+        component: Statistics
+      },
+      {
+        id: '2-4',
+        path: '/navigationTwo/statistics',
+        label: "Option 6",
+        component: Statistics
+      },
+      {
+        id: '2-5',
+        path: '/navigationTwo/statistics',
+        label: "Option 7",
+        component: Statistics
+      },
+    ]
   },
   {
-    id: 2,
-    path: '/home',
-    component: Home
-  },
-  {
-    id: 3,
-    path: '/assets',
-    component: Assets
-  },
-  {
-    id: 4,
-    path: '/statistics',
-    component: Statistics
+    id: '3',
+    icon: PieChartOutlined,
+    layout: UserLayout,
+    path: "navigationThree",
+    label: 'Navigation Three',
+    children: [{
+        id: '3-1',
+        path: '/navigationThree/mine',
+        label: 'Option 8',
+        component: Statistics
+      },
+      {
+        id: "3-2",
+        label: "Option 9",
+        path: '/navigationThree/home',
+        component: Statistics
+      },
+      {
+        id: '3-3',
+        path: '/navigationThree/assets',
+        label: "Option 10",
+        component: Statistics
+      }
+    ]
   },
 ]
 
